a5334be37bf4829606499b99c804ca66fcc58051
Sergey <92057970+sergstorozhev1984@users.noreply.github.com>
2023-01-22 21:55:04 +0200

    fulle

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,28 +1,22 @@
 import { ContactListItem } from './ContactListItem/ContactListItem';
 import css from './ContactList.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { getContactsFilter, getContacts } from 'redux/selectors';
+import { getFilteredContacts } from 'redux/selectors';
 import { fetchContacts } from 'redux/contacts.thunk';
 import { useEffect } from 'react';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(getContacts);
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
-  const filter = useSelector(getContactsFilter);
-
-  const filteredContacts = () =>
-    contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
+  const filteredContacts = useSelector(getFilteredContacts);
 
   return (
     <ul className={css.contactList}>
-      {filteredContacts().map(({ name, number, id }) => (
-        <ContactListItem key={id} name={name} number={number} id={id}/>
+      {filteredContacts.map(({ name, number, id }) => (
+        <ContactListItem key={id} name={name} number={number} id={id} />
       ))}
     </ul>
   );
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,10 @@
+import { createSelector } from "@reduxjs/toolkit";
+
+
 export const getContacts = state => state.contacts.items;
 export const getContactsFilter = state => state.filter;
 export const getIsloading = state => state.contacts.isLoading;
+export const getFilteredContacts = createSelector([getContacts, getContactsFilter], (contactsItems,filter) => {
+    return contactsItems.filter(el => el.name.toLowerCase().includes(filter.toLowerCase()));
+})
+
